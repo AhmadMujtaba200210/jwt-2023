cc03393044173590238da5c60b64e5b469f45e19
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-02-22 20:01:58 +0500

    feat: userdetails implemented

--- JWT/src/main/java/com/example/jwt/model/User.java
+++ JWT/src/main/java/com/example/jwt/model/User.java
@@ -7,12 +7,16 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity (name = "_user")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue (strategy=GenerationType.AUTO)
     private String id;
@@ -20,4 +24,34 @@ public class User {
     private String lastName;
     private String email;
     private String password;
+    private Role role;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
