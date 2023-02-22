90bbcf1b021f1ecb30fd1b3d0d85a17d0ad93f4e
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-02-22 20:53:49 +0500

    feat: returned required values

--- JWT/src/main/java/com/example/jwt/model/User.java
+++ JWT/src/main/java/com/example/jwt/model/User.java
@@ -1,16 +1,15 @@
 package com.example.jwt.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -19,39 +18,40 @@ import java.util.Collection;
 public class User implements UserDetails {
     @Id
     @GeneratedValue (strategy=GenerationType.AUTO)
-    private String id;
+    private int id;
     private String firstName;
     private String lastName;
     private String email;
     private String password;
+    @Enumerated(EnumType.STRING)
     private Role role;
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return email;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
