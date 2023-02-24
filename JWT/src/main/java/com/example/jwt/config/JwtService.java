1ca1c0f54003039892aff2e3b892ea4ad32e5879
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-02-24 17:54:48 +0500

    feat: getting signInKey using decoder

--- JWT/src/main/java/com/example/jwt/config/JwtService.java
+++ JWT/src/main/java/com/example/jwt/config/JwtService.java
@@ -2,10 +2,15 @@ package com.example.jwt.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+    private static final String SECRET_KEY="Secret";
     public String extractUsername(String token){
         return null;
     }
@@ -13,9 +18,14 @@ public class JwtService {
     private Claims extractAllClaims(String token){
         return Jwts
                 .parserBuilder()
-                .setSigningKey(getSignInKey())
+                .setSigningKey(getSignInKey()) // sign in key is used as digital signature same as the physical signature to insure level of trust also known as secret key
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    private Key getSignInKey(){
+        byte [] keyBytes= Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
