44870d98c027127e51334f15db4a32d32649deae
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-02-25 12:51:13 +0500

    chore: ApplicationConfig file updated

--- JWT/src/main/java/com/example/jwt/config/ApplicationConfig.java
+++ JWT/src/main/java/com/example/jwt/config/ApplicationConfig.java
@@ -1,2 +1,22 @@
-package com.example.jwt.config;public class ApplicationConfig {
+package com.example.jwt.config;
+
+import com.example.jwt.model.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+// TODO: why to write @Configuration annotation here? ;
+// bcz when app started this class will initiated at once for beans, this class contains all the beans of our application
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    private final UserRepository repository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> repository.findByEmail(username)
+                .orElseThrow(()->new UsernameNotFoundException("User not found"));
+    }
 }
