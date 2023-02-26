55911b0f76d5330a75701ab9cd1d618bad1c8328
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-02-26 22:25:10 +0500

    chore: extracting data from token models

--- JWT/src/main/java/com/example/jwt/auth/AuthenticationRequest.java
+++ JWT/src/main/java/com/example/jwt/auth/AuthenticationRequest.java
@@ -1,2 +1,14 @@
-package com.example.jwt.auth;public class AuthenticationRequest {
+package com.example.jwt.auth;
+
+import lombok.*;
+import org.springframework.stereotype.Service;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+    private String email;
+    private String passcode;
 }
