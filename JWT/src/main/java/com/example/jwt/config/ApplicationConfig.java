63a2fa55cd90eefa9fa22ba6b436544862490f3b
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-02-26 22:26:38 +0500

    feat: password encode and authentication manager bean created

--- JWT/src/main/java/com/example/jwt/config/ApplicationConfig.java
+++ JWT/src/main/java/com/example/jwt/config/ApplicationConfig.java
@@ -4,8 +4,14 @@ import com.example.jwt.model.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 // TODO: why to write @Configuration annotation here? ;
 // bcz when app started this class will initiated at once for beans, this class contains all the beans of our application
@@ -25,5 +31,15 @@ public class ApplicationConfig {
         daoAuthenticationProvider.setUserDetailsService(userDetailsService());
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
         return daoAuthenticationProvider;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+
+        return  config.getAuthenticationManager();
     }
 }
